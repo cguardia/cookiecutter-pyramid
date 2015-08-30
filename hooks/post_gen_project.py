5f820b0afa54a740e7330377e54a126ccccc0db9
Carlos de la Guardia <cguardia@yahoo.com>
2015-08-30 01:26:02 -0500

    add Pyramid's sorry for the convenience message after generation

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,7 +1,7 @@
 """ remove template files for all unselected engines and persistence options """
 import os
 import shutil
-
+from textwrap import dedent
 
 ENGINES = {'chameleon': 'pt', 'jinja2': 'jinja2', 'mako': 'mako'}
 SELECTED_ENGINE = '{{ cookiecutter.template_engine }}'
@@ -18,3 +18,16 @@ if SELECTED_PERSISTENCE not in PERSISTENCE_OPTIONS:
 
 if SELECTED_PERSISTENCE != 'sqlalchemy':
     shutil.rmtree('./{{ cookiecutter.repo_name }}/scripts')
+
+separator = "=" * 79
+msg = dedent(
+    """
+    %(separator)s
+    Tutorials: http://docs.pylonsproject.org/projects/pyramid_tutorials
+    Documentation: http://docs.pylonsproject.org/projects/pyramid
+    Twitter (tips & updates): http://twitter.com/pylons
+    Mailing List: http://groups.google.com/group/pylons-discuss
+    Welcome to Pyramid.  Sorry for the convenience.
+    %(separator)s
+""" % {'separator': separator})
+print msg
